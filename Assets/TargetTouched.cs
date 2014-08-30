fe2b8e01bef25bf3091216a9dacfb3fb319391b1
Thibault MERTZ <tmertz@e2r9p10.42.fr>
2014-08-31 01:34:17 +0200

    first push

---
+++ Assets/TargetTouched.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetTouched : MonoBehaviour {
+
+	public GameObject explosion;
+	private	Object expl;
+
+	IEnumerator	OnCollisionEnter(Collision target)
+	{
+		if (target.gameObject.name == "DeathGlidder")
+		{
+			expl = Instantiate(explosion, target.transform.position, target.transform.rotation);
+			Destroy(target.gameObject);
+			yield  return new WaitForSeconds(1);
+			Destroy(expl);
+		}
+	}
+}
